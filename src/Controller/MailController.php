b02e863c5d19577469e61952ae9380c09e50eb75
unknown <ziraouimohammed3@gmail.com>
2023-01-12 10:44:21 +0100

    Add Symfony Mailer

--- src/Controller/MailController.php
+++ src/Controller/MailController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Email;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,9 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MailController extends AbstractController
 {
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
+
     
     #[Route('/mail', name: 'app_mail')]
-    public function send(MailerInterface $mailer)
+    public function send()
     {
         $email = (new Email())
         ->from('mailtrap@example.com')
@@ -21,7 +30,28 @@ class MailController extends AbstractController
         ->text('Sending emails is fun again!')
         ->html('<p>See Twig integration for better HTML integration!</p>');
 
-        $mailer->send($email);
+        $this->mailer->send($email);
+
+        return $this->json(true);
+    }
+
+    #[Route('/mail/temp', name: 'app_mail')]
+    public function email_twig(ProductRepository $repo)
+    {
+        $email = (new TemplatedEmail())
+        ->from('fabien@example.com')
+        ->to('zira@gmail.com')
+        ->subject('Thanks for signing up!')
+        
+        // path of the Twig template to render
+        ->htmlTemplate('mail/template.html.twig')
+
+        // pass variables (name => value) to the template
+        ->context([
+            'product' => $repo->find(1)
+        ]);
+
+        $this->mailer->send($email);
 
         return $this->json(true);
     }
